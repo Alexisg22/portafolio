d1169810e032d3f90c5ff86cdbf0c328049b2431
Alexis <garciaciroalexis@gmail.com>
2025-12-05 23:12:53 -0500

    Correció de eslint

--- src/components/LanguageComponent.tsx
+++ src/components/LanguageComponent.tsx
@@ -4,7 +4,7 @@ import i18n from "../../public/locales/i18n/i18nConfig"
 
 export const LanguageComponent = () => {
     const [lang, setLang] = useState(i18n.language || "es")
-    const handlChange = (e:any) => {
+    const handleChange = (e:React.ChangeEvent<HTMLSelectElement>) => {
         setLang(e.target.value)
         i18n.changeLanguage(e.target.value)
     }
@@ -15,7 +15,7 @@ export const LanguageComponent = () => {
     }, [])
     return (
         <div className="w-1/6 flex justify-center items-center">
-            <select name="language"id="language"className="bg-zinc-800 rounded border border-sky-400 max-sm:size-5" value={lang} onChange={handlChange}>
+            <select name="language"id="language"className="bg-zinc-800 rounded border border-sky-400 max-sm:size-5" value={lang} onChange={handleChange}>
                 <option className="max-sm:size-5" value="es">ES</option>
                 <option className="max-sm:size-5" value="en">EN</option>
             </select>
